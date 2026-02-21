1178c292a125243407c22ad339f0fdc9592b95c1
SantiagoReyVarela <santi.varelarey@gmail.com>
2026-02-21 16:21:34 +0100

    puerta interactuable

--- ECOE sim/Assets/Scripts/Object/store/Door.cs
+++ ECOE sim/Assets/Scripts/Object/store/Door.cs
@@ -1,14 +1,52 @@
 using UnityEngine;
+using System.Collections;
 
 public class Door : ObjInteractive
 {
     private bool isOpen = false;
+    private bool isMoving = false;
+
+    [SerializeField] private Transform doorPivot;
+    [SerializeField] private float openAngle = -90f; 
+    //cambiarlo entre positivo/negativo si quieres que gire al otro lado
+
+    [SerializeField] private float speed = 5f;
+
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+
+    private void Start()
+    {
+        closedRotation = doorPivot.rotation;
+        openRotation = Quaternion.Euler(
+            doorPivot.eulerAngles + new Vector3(0, openAngle, 0)
+        );
+    }
 
     public override void Interact()
     {
-        if (!canInteract) return;
+        if (!canInteract || isMoving) return;
 
         isOpen = !isOpen;
-        Debug.Log(isOpen ? "Puerta abierta " : "Puerta cerrada ");
+        StartCoroutine(RotateDoor(isOpen ? openRotation : closedRotation));
+    }
+
+    private IEnumerator RotateDoor(Quaternion targetRotation)
+    {
+        isMoving = true;
+
+        while (Quaternion.Angle(doorPivot.rotation, targetRotation) > 0.1f)
+        {
+            doorPivot.rotation = Quaternion.Slerp(
+                doorPivot.rotation,
+                targetRotation,
+                Time.deltaTime * speed
+            );
+
+            yield return null;
+        }
+
+        doorPivot.rotation = targetRotation;
+        isMoving = false;
     }
 }
