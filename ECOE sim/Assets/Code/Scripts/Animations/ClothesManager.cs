629ef3b9bb55a71dd40c94c59b730b6e3453b19a
AntonioMB <anto293245@gmail.com>
2026-05-19 20:43:01 +0200

    Script para la Ropa (Prueba)

---
+++ ECOE sim/Assets/Code/Scripts/Animations/ClothesManager.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ClothType
+{
+    Cabeza,
+    Torso,
+    Piernas,
+    Pies,
+    Manos
+}
+
+public class ClothesManager : MonoBehaviour
+{
+    [Header("Cuerpo")]
+    [Tooltip("SkinnedMeshRenderer del cuerpo base")]
+    public SkinnedMeshRenderer smrBody;
+
+    private Dictionary<string, Transform> boneMap;
+    private Dictionary<ClothType, GameObject> equipedCloth = new Dictionary<ClothType, GameObject>();
+
+    void Start()
+    {
+        MapBaseRig();
+    }
+
+    private void MapBaseRig()
+    {
+        boneMap = new Dictionary<string, Transform>();
+
+        if (smrBody == null) return;
+
+        foreach (Transform bone in smrBody.bones)
+        {
+            if (!boneMap.ContainsKey(bone.name))
+            {
+                boneMap.Add(bone.name, bone);
+            }
+        }
+    }
+
+    public void EquipCloth(GameObject clothPrefab, ClothType cat)
+    {
+        if (clothPrefab == null) return;
+        if (boneMap == null || boneMap.Count == 0) MapBaseRig();
+
+        if (equipedCloth.ContainsKey(cat))
+        {
+            if (equipedCloth[cat] != null)
+            {
+                Destroy(equipedCloth[cat]);
+            }
+            equipedCloth.Remove(cat);
+        }
+
+        GameObject clothInstance = Instantiate(clothPrefab, transform);
+        SkinnedMeshRenderer smrCloth = clothInstance.GetComponentInChildren<SkinnedMeshRenderer>();
+
+        Transform[] newBones = new Transform[smrCloth.bones.Length];
+
+        for (int i = 0; i < smrCloth.bones.Length; i++)
+        {
+            string boneNameRequired = smrCloth.bones[i].name;
+
+            if (boneMap.TryGetValue(boneNameRequired, out Transform foundBone))
+            {
+                newBones[i] = foundBone;
+            }
+            else
+            {
+                Debug.LogWarning($"Hueso '{boneNameRequired}' requerido por la prenda no existe en el esqueleto base.");
+            }
+        }
+
+        smrCloth.bones = newBones;
+        smrCloth.rootBone = smrBody.rootBone;
+
+        Transform clothArmature = clothInstance.transform.Find("Armature");
+        if (clothArmature != null)
+        {
+            Destroy(clothArmature.gameObject);
+        }
+
+        equipedCloth.Add(cat, clothInstance);
+    }
+}
